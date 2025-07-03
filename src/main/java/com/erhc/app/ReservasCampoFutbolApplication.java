21895d66a9258384d52dfe4a73903aa2258c4c37
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-07-02 23:33:07 -0500

    chore: Configuración del inicio del sistema

---
+++ src/main/java/com/erhc/app/ReservasCampoFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.erhc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasCampoFutbolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservasCampoFutbolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erhc/app/ReservasCampoFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erhc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasCampoFutbolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
